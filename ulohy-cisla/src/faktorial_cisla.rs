a3aa786f44d60fa676f4a0a4870a982c639bbd59
Jan Pikl <fjpikl@gmail.com>
2026-01-07 12:05:47 +0100

    pridat faktorial cisla

---
+++ ulohy-cisla/src/faktorial_cisla.rs
@@ -0,0 +1,49 @@
+
+// 1. Součet prvních n přirozených čísel.
+
+use std::{io};
+
+pub fn run() {
+
+    println!("Napiš číslo, u kterého se má spočítat faktoriál:");
+
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Selhalo načtení čísla!");
+
+    let number: i32 = input.trim().parse().expect("Zadejte platné číslo!");
+
+    println!("\n");
+
+    // let result: i32 = faktorial_via_while(number);
+    let result: i32 = faktorial_via_for(number);
+
+
+    println!("\n Faktoriál čísla je: {result}");
+}
+
+fn faktorial_via_for(n: i32) -> i32 {
+
+    let mut number: i32 = 1;
+    for x in 1..n + 1 {
+        number = x * number;
+        // println!("{x}");
+    }
+
+    number
+}
+
+// fn faktorial_via_while(n: i32) -> i32 {
+
+//     let mut number: i32 = n;
+//     let mut result: i32 = 1;
+//     while number > 0 {
+//         result = result * number;
+//         println!("{number}");
+//         number -= 1;
+//     }
+
+//     result
+// }
--- ulohy-cisla/src/main.rs
+++ ulohy-cisla/src/main.rs
@@ -1,10 +1,12 @@
 // Součet prvních n přirozených čísel.
 
-mod soucet_prvnich_n_prirozenych_cisel;
+// mod soucet_prvnich_n_prirozenych_cisel;
+mod faktorial_cisla;
 
 fn main() {
 
-soucet_prvnich_n_prirozenych_cisel::run();
+// soucet_prvnich_n_prirozenych_cisel::run();
+faktorial_cisla::run();
 
 
 }
